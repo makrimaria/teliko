75f1d190024227ea6bdd396daf7df849ddad38ac
Maria Makri <mariam@mkinet.net>
2019-10-30 14:57:31 +0200

    sprint1 v2

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -22,12 +22,12 @@
                   <button type="button" class="btn btn-secondary wrn-btn">Search</button>
                 </div>
 
-                <div class="price">
+                <!-- <div class="price">
                   <slider :values="sliderValues" min="0" max="100" raising v-model="slider"></slider>
-                  <!-- remember to set v-model -->
+                  < remember to set v-model 
 
                   {{ slider }}
-                </div>
+                </div--> 
 
 
               </div>
@@ -40,47 +40,14 @@
 </template>
 
 <script>
-  import Slider from "vue-custom-range-slider";
-  // import the styling, css or scss
-  import "vue-custom-range-slider/dist/vue-custom-range-slider.css";
 
-  export default {
-    components: {
-      Slider
-    },
-    data() {
-      return {
-        slider: "0",
-        sliderValues: [
-          {
-            label: "Not at all",
-            value: "0"
-          },
-          {
-            label: "A tiny bit",
-            value: "1"
-          },
-          {
-            label: "Its ok",
-            value: "2"
-          },
-          {
-            label: "Its very good",
-            value: "3"
-          },
-          {
-            label: "Its AMAZING!",
-            value: "4"
-          }
-        ]
-      };
-    }
-  };
+  export default { }
+    
 </script>
 
 <style>
 .search-sec {
-  padding: 15px;
+  padding: 5px;
 }
 .search-slt {
   display: block;
@@ -106,7 +73,7 @@
   .search-sec {
     position: relative;
     top: -114px;
-    background: rgba(0, 123, 255, 0.35);
+    background: rgba(0, 123, 255, 0);
   }
 }
 
