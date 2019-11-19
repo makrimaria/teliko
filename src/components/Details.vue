8e7b3b0ec3405609aa3641a8962a3223b88c90e1
sidchristos <sidchristos_2@hotmail.com>
2019-11-19 16:04:12 +0200

    filter with url query

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,44 +1,52 @@
 <template>
-    <div class="container">
-      
-      <b-list-group>
-        <b-img class="card" :src="house.image"></b-img>
-        <b-list-group-item>{{house.data.city}}</b-list-group-item>
-        <b-list-group-item>{{house.data.region}}</b-list-group-item>
-        <b-list-group-item>{{house.data.area}}</b-list-group-item>
-        <b-list-group-item>{{house.data.price}}</b-list-group-item>
-      </b-list-group>
-    </div>
+  <div class="container">
+    <b-list-group>
+      <b-img class="card" :src="house[0].image"></b-img>
+      <b-list-group-item>{{house[0].city}}</b-list-group-item>
+      <b-list-group-item>{{house[0].region}}</b-list-group-item>
+      <b-list-group-item>{{house[0].area}}</b-list-group-item>
+      <b-list-group-item>{{house[0].price}}</b-list-group-item>
+      <b-list-group-item>{{house[0].type}}</b-list-group-item>
+      <b-list-group-item v-if="house[0].rent == true">Rent</b-list-group-item>
+      <b-list-group-item v-else>Sale</b-list-group-item>
+    </b-list-group>
+    <Dummy></Dummy>
+  </div>
 </template>
 
 <script>
-import {dbfs} from "../config/db";
+import Dummy from "./Dummy";
+import { dbfs } from "../config/db";
+
 export default {
+  name: "Details",
+  components: {
+    Dummy
+  },
   data() {
-    return{
-     house: []
-    }
+    return {
+      house: []
+    };
   },
   mounted() {
-    this.house = this.$route.params.house
-  },
-  updated() {
     dbfs
-      .collection("houses/"+ this.house.id)
+      .collection("houses")
+      .doc(this.$route.query.id)
       .get()
-      .then(querySnapshot => {
-        querySnapshot.docs.forEach(doc => {
-          this.houses.push({id: doc.id, data: doc.data()});
-        });
+      .then(doc => {
+        if (doc && doc.exists) {
+          this.house.push(doc.data());
+        }
       });
+      console.log(this.house)
   }
-}
+};
 </script>
 
 <style scoped>
-.card, :hover {
+.card,
+:hover {
   max-width: -webkit-fill-available;
   color: black;
 }
-
 </style>
---
+++ src/variants.js
@@ -0,0 +1,30 @@
+import { dbfs } from './config/db'
+
+export var ci = []
+export var re = []
+
+//populate dropdown list
+var i = 0;
+dbfs
+  .collection("cities")
+  .orderBy("name", "asc")
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.docs.forEach(doc => {
+      ci.push({ value: i, text: doc.data().name });
+      i++;
+    });
+  });
+var j = 0;
+dbfs
+  .collection("cities")
+  .orderBy("name", "asc")
+
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.docs.forEach(doc => {
+      re.push({ value: j, text: doc.data().regions });
+      j++;
+    });
+  });
+
