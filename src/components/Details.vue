05185d1f24bb14dc9948ac96c13ff446a9d5bdd7
makrimaria <49186060+ makrimaria@users.noreply.github.com>
2019-11-25 21:08:21 +0200

    !

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -6,9 +6,41 @@
       <!-- <div class="row"> -->
       <div class="column">
         <div class="container">
+
+          
+
+          <!-- <b-list-group-item class="titleof">
+            {{house.data.rent}}, {{house.data.type}} {{house.data.area}}, {{house.data.location}}, {{house.data.city}} </b-list-group-item> -->
+    <!-- carousel -->
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="width:100%; height:600px; ">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" :src="house[0].image" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" :src="house[0].image2" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" :src="house[0].image3" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
           <!--eikones-->
           <div class="table2">
-            <b-img class="card" :src="house[0].image" style="width:inherit; height:auto;"></b-img>
+            <!-- <b-img class="card" :src="house[0].image" style="width:inherit; height:auto;"></b-img> -->
             <br />
             <h3 style="text-align:center; font-weight:900;">Details</h3>
             <hr />
@@ -31,8 +63,8 @@
         </div>
       </div>
       <div class="column">
-        <b-img class="card" :src="house[0].image2" style="width:inherit; height:auto;"></b-img>
-        <b-img class="card" :src="house[0].image3" style="width:inherit; height:auto;"></b-img>
+        <!-- <b-img class="card" :src="house[0].image2" style="width:inherit; height:auto;"></b-img>
+        <b-img class="card" :src="house[0].image3" style="width:inherit; height:auto;"></b-img> -->
         <br />
         <br />
         <h4 style="font-weight:900; text-align:left;">Additional info</h4>
@@ -116,6 +148,8 @@ export default {
   width: inherit;
   height: inherit;
 }
+
+
 /* Clear floats after the columns */
 /* .row:after {
   content: "";
